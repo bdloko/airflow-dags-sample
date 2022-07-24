c4b255db16e10419bc5b401b8c09e312d8636431
Buntu Dloko <mrdloko@gmail.com>
2022-07-24 13:23:47 +0200

    secont commit

--- test_dag.py
+++ test_dag.py
@@ -20,7 +20,7 @@ This dag only runs some simple tasks to test Airflow's task execution.
 """
 from datetime import datetime, timedelta
 
-from airflow.models.dag import DAG
+from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.utils.dates import days_ago
 
